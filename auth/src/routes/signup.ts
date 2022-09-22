d377e5dfc31bc9dc67cf0a544605b3ff5bc6351f
SilvesterSpath <spath.sz@gmail.com>
2022-09-22 08:20:47 +0200

    express-validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import {body} from 'express-validator'
 
 const router = express.Router()
 
 router.post('/api/users/signup', (req, res)=>{
-  res.send('Sign up!')
+  const { email, password } = req.body
+
+  if(!email || typeof email !== 'string'){
+    res.status(400).send('Provide a valid email')
+  }
+
+  // new User({ email, password })
+  const user = {email, password}
+
+
+  res.status(201).send(user)
 })
 
 export { router as signupRouter }
