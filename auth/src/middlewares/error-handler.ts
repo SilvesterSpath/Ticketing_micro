7b8e2bbb291b160636255583517abfc8d6d103e7
SilvesterSpath <spath.sz@gmail.com>
2022-09-26 08:12:03 +0200

    error-handler formatted response

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from "express"
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorhandler = (err: Error, req: Request, res: Response, next: NextFunction)=>{
-  console.log('Something went wrong', err);
+  if(err instanceof RequestValidationError){
+    const formattedError = err.errors.map(item =>{
+      return {
+        message: item.msg,
+        field: item.param
+       }
+    })
+    return res.status(400).send({errors: formattedError})        
+  }
+
+  if( err instanceof DatabaseConnectionError){
+    console.log('this is a database connection error')
+  }
+  
 
   res.status(400).send({
     message: err.message
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express'
 import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 const router = express.Router()
 
@@ -8,22 +10,20 @@ router.post('/api/users/signup', [
   body('password').trim().isLength({min: 4, max:20}).withMessage('Password must be between 4 and 20 characters')
 ],
  (req: Request, res: Response)=>{
-  const errors = validationResult(req)
+  const errors = validationResult(req) // this is an object  
 
   if(!errors.isEmpty()){
     /* return res.status(400).send(errors.array()) */
-    // This is JavaScript land, not TypeScript
-    const error = new Error('Invalid email or password')
-    
-    throw error;
+    throw new RequestValidationError(errors.array())
   }
 
   const { email, password } = req.body
 
   console.log('Creating a user..')
+  throw new DatabaseConnectionError()
  
   // new User({email, password})
-  const user = undefined
+  const user = {email, password}
 
   if(!user || user === undefined){
     throw new Error('Error connecting to database')
