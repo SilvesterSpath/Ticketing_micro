d82bcd0194da2e11a68dd1962e54e8579d8e7072
SilvesterSpath <spath.sz@gmail.com>
2022-09-29 10:03:58 +0200

    User.build

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,6 +7,12 @@ interface UserAttrs {
   password: string
 }
 
+// Interface that describes the properties
+// that a User model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any
+}
+
 const userSchema = new mongoose.Schema({
   email:{
     type: String,
@@ -18,22 +24,27 @@ const userSchema = new mongoose.Schema({
   }
 })
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+// This is how a custom function built into a model now we can use 'User.build'
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
 
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+User.build({
+  email: 'test@test.com',
+  password:'12345'  
+})
+
 // build user with the help of typescript through the interface
-/* buildUser({
-  email: 'tes@test.com',
-  password: '12345',
-}) */
+/* const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs)
+} */
 
 /* new User({
   email: 'tes@test.com',
   password: '12345'
 }) */
 
-export { User, buildUser }
+export { User }
 
