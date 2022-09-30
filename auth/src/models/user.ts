cf38971683c97ff6bd63083b8dec5f218ab7181c
SilvesterSpath <spath.sz@gmail.com>
2022-09-30 08:56:49 +0200

    UserSchema with typescript

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,8 +9,17 @@ interface UserAttrs {
 
 // Interface that describes the properties
 // that a User model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc
+}
+
+// Interface that describes the properties
+// that a User Document has in mongoDB (this could have additional properties like 'createdAt')
+interface UserDoc extends mongoose.Document{
+  email: string,
+  password: string,
+/*   createdAt: string,
+  updateAt: string */
 }
 
 const userSchema = new mongoose.Schema({
@@ -29,7 +38,7 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 User.build({
   email: 'test@test.com',
@@ -40,11 +49,6 @@ User.build({
 /* const buildUser = (attrs: UserAttrs) => {
   return new User(attrs)
 } */
-
-/* new User({
-  email: 'tes@test.com',
-  password: '12345'
-}) */
 
 export { User }
 
