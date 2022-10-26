a62440f35d32b60a0d8d60cb4b60677d349e3f63
SilvesterSpath <spath.sz@gmail.com>
2022-10-26 06:42:27 +0200

    validate-request middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express'
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error'
+import { body } from 'express-validator'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
@@ -11,13 +11,9 @@ router.post('/api/users/signin', [
   body('password')
     .trim()
     .notEmpty()
-    .withMessage('Must apply a password')
-], (req: Request, res: Response)=>{  
-  const errors = validationResult(req)  
+    .withMessage('Must supply a password')
+], validateRequest, (req: Request, res: Response)=>{  
 
-  if(!errors.isEmpty()){    
-    throw new RequestValidationError(errors.array())
-  }
   
 })
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,23 +1,23 @@
 import express, {Request, Response} from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
 import jwt from 'jsonwebtoken'
 import { User } from '../models/user'
-import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
 router.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({min: 4, max:20}).withMessage('Password must be between 4 and 20 characters')
-],
+], validateRequest,
  async(req: Request, res: Response)=>{
-  const errors = validationResult(req) // this is an object  
+/*   const errors = validationResult(req) // this is an object  
 
   if(!errors.isEmpty()){
-    /* return res.status(400).send(errors.array()) */
+    // return res.status(400).send(errors.array()) 
     throw new RequestValidationError(errors.array())
-  }
+  } */
 
   const { email, password } = req.body
 
