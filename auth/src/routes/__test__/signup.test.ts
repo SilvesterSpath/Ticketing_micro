3d11319b0a8bd9699f2a8e1075d1b2c501bf7cc1
SilvesterSpath <spath.sz@gmail.com>
2022-11-21 07:34:51 +0100

    signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,56 @@ it('returns a 201 on successful signup', async ()=>{
     })
     .expect(201)
 })
+
+it('returns a 400 statuscode with invalid email', async()=>{
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testest.com',
+      password: 'password'
+    })
+    .expect(400)
+})
+
+it('returns a 400 statuscode with invalid password', async()=>{
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'tes@test.com',
+      password: 'pd'
+    })
+    .expect(400)
+})
+
+it('returns a 400 statuscode with missing email or password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'tes@test.com'
+    })
+    .expect(400)
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'asdfasdfsadf'
+    })
+    .expect(400)
+})
+
+it('dissallows duplicate emails', async()=>{
+  await request(app)
+  .post('/api/users/signup')
+  .send({
+    email: 'tes@test.com',
+    password: 'password'
+  })
+  .expect(201)
+await request(app)
+  .post('/api/users/signup')
+  .send({
+    email: 'tes@test.com',
+    password: 'password'
+  })
+  .expect(400)
+})
+
