00b029caf4597a58df088ff0983c65dfe9585e09
SilvesterSpath <spath.sz@gmail.com>
2023-03-23 06:31:24 +0100

    getInitialProps()

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,18 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = (/* { color } */ { currentUser }) => {
+  /*  console.log('I am in the component!', color); */
+  console.log(currentUser);
+  axios.get('/api/users/currentuser');
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  /*   console.log('I am on the server!');
+  return { color: 'red' }; */
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
